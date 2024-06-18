4d0e9808e1b11b02d01d51d30d159a93ec6044ad
Vitor Veras <vitorverasm@gmail.com>
2024-06-18 19:08:42 -0300

    feat: call user handler on route /users

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,25 @@
 import fastify from "fastify";
+import { makeUserHandler } from "../modules/user/main/user-handler-factory";
+import { makeUserRepository } from "../modules/user/main/user-repository-factory";
 
 export function startHTTPServer(port: number) {
     const server = fastify();
+    const userHandler = makeUserHandler(makeUserRepository())
 
-    server.get("/", () => {
-        return { message: "Hello, world!" }
+    server.get("/", (_, response) => {
+        return response.code(200).send({ message: "Hello world" })
+    })
+
+    server.get("/users", async (_, response) => {
+        try {
+            const users = await userHandler.getAllUsers()
+            return response.code(200).send({ users, message: "Users list" })
+        } catch (error) {
+            if (error instanceof Error) {
+                console.log("/users [error]: ", error.message)
+            }
+            return response.code(400).send({ users: [], message: "Failed to get users" })
+        }
     })
 
     server.listen({ port, host: "0.0.0.0" }).then(() => {
